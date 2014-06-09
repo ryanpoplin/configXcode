a197c8e7aec1d77b8727173ee48ca3a500bacaf5
Byrdann Fox <mobileupstate@gmail.com>
2014-06-09 19:21:35 -0400

    Just testing Obj-C stuff...

--- TImeTime/ECTCalculator.m
+++ TImeTime/ECTCalculator.m
@@ -10,4 +10,35 @@
 
 @implementation ECTCalculator
 
+{
+ 
+    double total;
+
+}
+
+- (void) clear
+{
+    total = 0.00;
+}
+- (double)addToTotal:(double)value
+{
+    total += value;
+    return total;
+}
+- (double)subtractFromTotal:(double)value
+{
+    total -= value;
+    return total;
+}
+- (double)multiplyTotal:(double)value
+{
+    total *= value;
+    return total;
+}
+- (double)divideTotal:(double)value
+{
+    total /= value;
+    return total;
+}
+
 @end
