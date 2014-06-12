ed66c01f638e341e1970e5c3c47470489a052994
Byrdann Fox <mobileupstate@gmail.com>
2014-06-12 04:15:30 -0400

    Nice...

--- TImeTime/ECTViewController.m
+++ TImeTime/ECTViewController.m
@@ -13,9 +13,10 @@
     int afterRemainder;
     int remainder;
     int bgConSum;
-    //...
+    int threeFourthsWay;
     int halfWay;
-    //...
+    int quarterWay;
+    NSTimer *autoTimer;
     NSTimeInterval countDownInterval;
 }
 @end
@@ -23,17 +24,22 @@
 @implementation ECTViewController
 
 - (void)viewDidLoad
+
 {
+    
     [super viewDidLoad];
     
-    NSString *excepappsString = @"by ExcepApps";
-    
-    self.excepappsString.text = excepappsString;
+    // NSString *excepappsString = @"by ExcepApps";
+    // self.excepappsString.text = excepappsString;
+
 }
 
 - (void)didReceiveMemoryWarning
+
 {
+    
     [super didReceiveMemoryWarning];
+    
 }
 
 - (IBAction)startButton:(id)sender {
@@ -41,30 +47,42 @@
     remainder = countDownInterval;
     afterRemainder = countDownInterval - remainder%60;
     bgConSum = afterRemainder;
-    //...
+    threeFourthsWay = bgConSum * 0.75;
     halfWay = bgConSum / 2;
-    //...
-    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateCountDown) userInfo:nil repeats:YES];
+    quarterWay = bgConSum * 0.25;
+    autoTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateCountDown) userInfo:nil repeats:YES];
 }
 
 - (void)updateCountDown {
     NSLog(@"%d", afterRemainder);
     afterRemainder --;
-    if (afterRemainder > halfWay) {
-        [UIView animateWithDuration:3.0 animations:^{
+    if (afterRemainder > threeFourthsWay) {
+        [UIView animateWithDuration:1.0 animations:^{
             self.view.backgroundColor = [UIColor greenColor];
         }];
         [UIView commitAnimations];
-    } else if (afterRemainder > 0) {
-        [UIView animateWithDuration:3.0 animations:^{
+    } else if (afterRemainder > halfWay) {
+        [UIView animateWithDuration:1.0 animations:^{
+            self.view.backgroundColor = [UIColor blueColor];
+        }];
+        [UIView commitAnimations];
+    } else if (afterRemainder > quarterWay) {
+        [UIView animateWithDuration:1.0 animations:^{
             self.view.backgroundColor = [UIColor yellowColor];
         }];
         [UIView commitAnimations];
-    } else if (afterRemainder <= 0) {
-        [UIView animateWithDuration:3.0 animations:^{
+    } else if (afterRemainder > 0) {
+        [UIView animateWithDuration:1.0 animations:^{
+            self.view.backgroundColor = [UIColor orangeColor];
+        }];
+        [UIView commitAnimations];
+    } else if (afterRemainder == 0) {
+        [UIView animateWithDuration:1.0 animations:^{
             self.view.backgroundColor = [UIColor redColor];
         }];
         [UIView commitAnimations];
+        [autoTimer invalidate];
+        autoTimer = nil;
     }
     int hours = (int)(afterRemainder/(60*60));
     int mins = (int)(((int)afterRemainder/60)-(hours*60));
@@ -78,7 +96,6 @@
     } else if (hours == 0 && mins == 0 && secs >= 0) {
         NSString *displayText = [[NSString alloc] initWithFormat:@"%2u s", secs];
         self.displayLabel.text = displayText;
-        // When 'secs' is zero, you need to stop the countdown timer...
     }
 }
 
