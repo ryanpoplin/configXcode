c6c330f6cc9c01a5e89cce2af962df2835f08758
Byrdann Fox <mobileupstate@gmail.com>
2014-07-22 19:27:33 -0400

    first commit

---
+++ TImeTime/ECTPacManLayer.h
@@ -0,0 +1,13 @@
+//
+//  ECTPacManLayer.h
+//  Color Countdown
+//
+//  Created by Byrdann Fox on 7/22/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface ECTPacManLayer : CAShapeLayer
+
+@end
---
+++ TImeTime/ECTPacManLayer.m
@@ -0,0 +1,13 @@
+//
+//  ECTPacManLayer.m
+//  Color Countdown
+//
+//  Created by Byrdann Fox on 7/22/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import "ECTPacManLayer.h"
+
+@implementation ECTPacManLayer
+
+@end
