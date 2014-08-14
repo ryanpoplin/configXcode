16db7a68b83efa566893984e79911178f6c7d51c
Byrdann Fox <mobileupstate@gmail.com>
2014-08-14 12:54:10 -0400

    Updates...

--- TImeTime/ECTAppDelegate.m
+++ TImeTime/ECTAppDelegate.m
@@ -28,8 +28,6 @@ NSDate *lastMagicMoment;
     
     logicGate = false;
     
-    // NSLog(@"APP HAS RESIGNED ACTIVE...");
-    
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
@@ -38,8 +36,6 @@ NSDate *lastMagicMoment;
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     
-    // NSLog(@"APP HAS ENTERED BACKGROUND...");
- 
     logicGate = true;
     
     if (afterRemainder && pauseTracker != 0 && logicGate == true) {
@@ -60,16 +56,12 @@ NSDate *lastMagicMoment;
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     
-    // NSLog(@"APP WILL ENTER FOREGROUND...");
-    
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    
-    // NSLog(@"APP DID BECOME ACTIVE...");
     
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
@@ -84,16 +76,14 @@ NSDate *lastMagicMoment;
     }
     
     if (lastMagicMoment == nil) {
-        
-        // NSLog (@"First launch!");
+
+        // do nothing...
         
     } else {
         
         if (afterRemainder && pauseTracker > 0 && logicGate == true) {
             
             backgroudTime = (int)timeOfNoMagic;
-            
-            // NSLog(@"Application was in background for %f...\n", backgroudTime);
             
         } else {
             
@@ -127,8 +117,6 @@ NSDate *lastMagicMoment;
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    
-    // NSLog(@"APP WILL TERMINATE...");
     
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     
